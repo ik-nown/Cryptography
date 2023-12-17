64a0af2d9f0ade9688021d7937cfc38228f9f74e
NGUYEN THI QUYNH ANH <127226698+winndy112@users.noreply.github.com>
2023-12-18 02:00:09 +0700

    update 5.2

--- task5/5.2/5.2.cpp
+++ task5/5.2/5.2.cpp
@@ -1,147 +1,147 @@
-// CPP standard library
-#include <iostream>
-#include <windows.h>
-#include <fstream>
-#include <locale>
-
-// OpenSSL library
 #include <openssl/bio.h>
 #include <openssl/x509.h>
 #include <openssl/pem.h>
-#include <openssl/asn1.h>
+#include <iostream>
+#include <openssl/err.h>
 
-using namespace std;
+bool verifyCertificate(X509* cert, STACK_OF(X509)* intermediateCerts, X509_STORE* store) {
+    // Create a verification context
+    X509_STORE_CTX* ctx = X509_STORE_CTX_new();
+    X509_STORE_CTX_init(ctx, store, cert, intermediateCerts);
 
-void printValidity(X509* x509)
-{
-    ASN1_TIME* notBefore = X509_get_notBefore(x509);
-    ASN1_TIME* notAfter = X509_get_notAfter(x509);
-    std::cout << "Validity: \n";
-    if (notBefore)
-    {
-        BIO* bio = BIO_new(BIO_s_mem());
-        ASN1_TIME_print(bio, notBefore);
-        char* buffer;
-        long length = BIO_get_mem_data(bio, &buffer);
+    // Verify the certificate
+    int result = X509_verify_cert(ctx);
 
-        std::cout << "Not Before: " << std::string(buffer, length) << std::endl;
+    // Clean up
+    X509_STORE_CTX_free(ctx);
 
-        BIO_free(bio);
-    }
-
-    if (notAfter)
-    {
-        BIO* bio = BIO_new(BIO_s_mem());
-        ASN1_TIME_print(bio, notAfter);
-        char* buffer;
-        long length = BIO_get_mem_data(bio, &buffer);
-
-        std::cout << "Not After : " << std::string(buffer, length) << std::endl;
-
-        BIO_free(bio);
-    }
+    return result == 1;
 }
 
-void printSignature(X509* x509)
-{
-    // Get the signature algorithm
-    const X509_ALGOR* sigAlg = X509_get0_tbs_sigalg(x509);
-    const ASN1_OBJECT* sigObj = nullptr;
-    int sigType = 0;
-    const void* sigValue = nullptr;
+void printPublicKeyHex(X509* cert) {
+    EVP_PKEY* pubKey = X509_get_pubkey(cert);
+    if (pubKey) {
+        std::cout << "Subject Public Key Info:" << std::endl;
+        int keyType = EVP_PKEY_id(pubKey);
+        std::cout << "  Public Key Algorithm: " << OBJ_nid2ln(keyType) << std::endl;
+        int keyBits = EVP_PKEY_bits(pubKey);
+        std::cout <<"       Public-Key: " << keyBits << " bits \n";
+        // Print the public key in HEX format
+        BIO* bio = BIO_new(BIO_s_mem());
+        PEM_write_bio_PUBKEY(bio, pubKey);
 
-    X509_ALGOR_get0(&sigObj, &sigType, &sigValue, sigAlg);
+        BUF_MEM* bptr;
+        BIO_get_mem_ptr(bio, &bptr);
 
-    std::cout << "Signature Algorithm: ";
-    if (sigObj)
-    {
-        const char* sigAlgName = OBJ_nid2ln(OBJ_obj2nid(sigObj));
-        if (sigAlgName)
-        {
-            std::cout << sigAlgName << std::endl;
-            // OPENSSL_free((void*)sigAlgName);
-        }
-    }
-
-    //Get the signature
-    const ASN1_BIT_STRING* signature = nullptr;
-    X509_get0_signature(&signature, &sigAlg, x509);
-    std::cout << "Signature:" << std::endl;
-    if (signature)
-    {   int count = 0;
-        for (int i = 0; i < signature->length; ++i)
-        {
-            printf("%02X", signature->data[i]);
-            count ++;
-            if (i != signature->length -1)
-            {
-                std::cout <<":";
+        for (size_t i = 0; i < bptr->length; ++i) {
+            printf("%02X", static_cast<unsigned char>(bptr->data[i]));
+            if (i < bptr->length - 1) {
+                if ((i + 1) % 32 == 0) {
+                    std::cout << std::endl;
+                } else {
+                    std::cout << ":";
+                }
             }
-            if (count == 18) {
-                std::cout << std::endl;
-                count = 0;
-            }
-
         }
-        
+
+        std::cout << std::endl;
+
+        BIO_free(bio);
+        EVP_PKEY_free(pubKey);
+    } else {
+        std::cerr << "Failed to retrieve the public key." << std::endl;
     }
 }
-
-int main (int argc, char* argv[])
-{   
-
-//support for Vietnamese 
-#ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-#endif
+int main() {
+    // Initialize OpenSSL
     OpenSSL_add_all_algorithms();
-    std::cout<<"Choose your file format (DER/PEM): (1/2)\n";
-    int choice; cin>>choice;
-    std::cout<<"Enter your file name: ";
-    string path; cin>>path;
-    // READ CERTIFICATE FILE
-    BIO* bio = BIO_new(BIO_s_file());
-    BIO_read_filename(bio, path.c_str());
-    X509* x509 = nullptr;
+    ERR_load_BIO_strings();
+    ERR_load_crypto_strings();
+    int choice;
+    std::cout << "Enter cert's file format (1-DER/2-PEM): "; std::cin >> choice;
+    std::string RootCACert, IntermediateCert, WebsiteCert;
+    // Load the Root CA Cert
+    std::cout << "Enter your RootCA Cert file: "; std::cin >> RootCACert;
+    BIO* bioRoot = BIO_new(BIO_s_file());
     
+    BIO_read_filename(bioRoot, RootCACert.c_str());
+    X509* rootCACert = nullptr;
     if (choice == 1) {
-        x509 = d2i_X509_bio(bio, nullptr);
+        rootCACert = d2i_X509_bio(bioRoot, nullptr);
     }
     else if (choice == 2) {
-        x509 = PEM_read_bio_X509(bio, nullptr, nullptr, nullptr);
+        rootCACert = PEM_read_bio_X509(bioRoot, nullptr, nullptr, nullptr);
     }
     else {
-        cout<<"Invalid choosen.\n"; 
+        std::cout << "Can't open " <<  RootCACert << std::endl;
+        BIO_free(bioRoot);
+        return -1;
     }
-    if (!x509) {
-        std::cerr << "Error reading X.509 certificate." << std::endl;
-        BIO_free(bio);
-        return false;
-        }
-    BIO_free(bio);
+    BIO_free(bioRoot);
+    //std::cout << "test 2" << std::endl;
 
-    // Print the subject name and issuer name
-    std::cout << "Subject Name: " << X509_NAME_oneline(X509_get_subject_name(x509), nullptr, 0) << std::endl;
-    std::cout << "Issuer Name: " << X509_NAME_oneline(X509_get_issuer_name(x509), nullptr, 0) << std::endl;
-    //print time of certificate
-    printValidity(x509);
-    cout<< endl;
-    // Print siganture
-    printSignature(x509);
-    cout << std::endl;
-    EVP_PKEY* publicKey = X509_get_pubkey(x509);
-    // verify the signature
-    int signatureValid = X509_verify(x509, publicKey);
-    if (signatureValid == 1) {
-        // print the public key
-        EVP_PKEY_print_public_fp(stdout, publicKey, 0, nullptr);
-        cout << std::endl;
+    // Load the Intermediate Cert
+    std::cout << "Enter your Intermediate Cert file: "; std::cin >> IntermediateCert;
+    BIO* intermediateFile = BIO_new(BIO_s_file());
+    BIO_read_filename(intermediateFile, IntermediateCert.c_str()); // Use intermediateFile instead of bioRoot
+    X509* intermediateCert = nullptr;
+    STACK_OF(X509)* intermediateCerts = sk_X509_new_null();
+    
+    if (choice == 1) {
+        intermediateCert = d2i_X509_bio(intermediateFile, nullptr);
+    }
+    else if (choice == 2) {
+        intermediateCert = PEM_read_bio_X509(intermediateFile, nullptr, nullptr, nullptr);
     }
     else {
-        cout << "Invalid signature." << endl;
+        std::cout << "Can't open " <<  IntermediateCert << std::endl;
+        BIO_free(intermediateFile);
+        return -1;
     }
+    sk_X509_push(intermediateCerts, intermediateCert);
+    BIO_free(intermediateFile);
+
+    // Load the website's Cert
+    std::cout << "Enter your website Cert file: "; std::cin >> WebsiteCert;
+    BIO* bioWebsite = BIO_new(BIO_s_file());
+    BIO_read_filename(bioWebsite, WebsiteCert.c_str());
+    X509* websiteCert = nullptr;
+    if (choice == 1) {
+        websiteCert = d2i_X509_bio(bioWebsite, nullptr);
+    }
+    else if (choice == 2) {
+        websiteCert = PEM_read_bio_X509(bioWebsite, nullptr, nullptr, nullptr);
+    }
+    else {
+        std::cout << "Can't open " <<  WebsiteCert << std::endl;
+        BIO_free(bioWebsite);
+        return -1;
+    }
+   
+
+    // Create a certificate store and add Root CA Cert
+    X509_STORE* store = X509_STORE_new();
+    X509_STORE_add_cert(store, rootCACert);
+
+    // Verify the website's certificate
+    bool isValid = verifyCertificate(websiteCert, intermediateCerts, store);
+
+    // Print the result
+    if (isValid) {
+        std::cout << "Website certificate is valid. \n" << std::endl;
+        std::cout << "Subject Name: " << X509_NAME_oneline(X509_get_subject_name(websiteCert), nullptr, 0) << std::endl;
+        std::cout << "Issuer Name: " << X509_NAME_oneline(X509_get_issuer_name(websiteCert), nullptr, 0) << std::endl;
+        printPublicKeyHex(websiteCert);
+    } else {
+        std::cerr << "Website certificate verification failed." << std::endl;
+    }
+
     // Clean up
-    X509_free(x509);
-    EVP_PKEY_free(publicKey);
+    sk_X509_pop_free(intermediateCerts, X509_free);
+    BIO_free(bioWebsite);
+    X509_free(rootCACert);
+    X509_free(websiteCert);
+    X509_STORE_free(store);
+
+    return 0;
 }
